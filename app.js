88ff5019423653e84c9607cd1136e29ea9279cac
Jagpat Singh <jsingh14192@gmail.com>
2016-08-30 17:03:09 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function isVampireNumber(num){
+  var fangs = [];
+  var factors = [];
+  var homoFactors = []; // Factors with same number of digits
+  var numToString = num.toString().split("");
+  
+}
